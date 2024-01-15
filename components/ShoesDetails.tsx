df338c0fe2a9be36eb13b47497efb3d262ff049f
Muzammal-Rafique <muzammalrafique26@gmail.com>
2024-01-16 02:27:46 +0500

    Complete Shoes details page

--- components/ShoesDetails.tsx
+++ components/ShoesDetails.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface ProductProps {
@@ -18,32 +20,42 @@ const ShoesDetails = ({
   image,
   rating,
 }: ProductProps) => {
+  const router = useRouter();
+
   return (
     <div>
-      <div className="hero min-h-screen ">
+      <div className="hero min-h-screen shadow-2xl ">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <Image
             src={image}
             alt="shoes"
             width={1000}
-            height={1000}
-            className="max-w-sm rounded-lg shadow-2xl"
+            height={900}
+            className="max-w-sm rounded-lg shadow-2xl bg-[#e79d3b]"
           />
-          <div>
-            <h1 className="text-5xl font-bold">{title}</h1>
+          <div className="w-1/2">
+            <h1 className="text-5xl font-bold text-[#FF6452] ">{title}</h1>
             <p className="py-6">{description}</p>
-            <div className=" flex justify-start gap-2.5">
+            <p className="font-semibold font-montserrat text-[#FF6452] text-2xl leading-normal pt-5">
+              <span className="text-black">Price:</span> {price} $
+            </p>
+            <div className=" flex justify-start gap-2.5 pt-8">
               <Image src="/star.svg" alt="rating icon" width={24} height={24} />
               <p className="font-montserrat text-xl leading-normal text-slate-gray ">
                 {`(${rating})`}
               </p>
             </div>
-            <p className="font-semibold font-montserrat text-[#FF6452] text-2xl leading-normal pt-10">
-              $ {price}
-            </p>
             <div className="card-actions justify-start pt-10">
               <button className="btn btn-info">Add Cart</button>
               <button className="btn btn-success">Buy Now</button>
+            </div>
+            <div className="flex pt-5">
+              <button
+                className="btn btn-error"
+                onClick={() => router.push("/products")}
+              >
+                View More
+              </button>
             </div>
           </div>
         </div>
